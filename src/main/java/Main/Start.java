f6fa135ce9588e75db597ca28079030209b39b21
Mohammed Deifallah <mohammed_deifallah@yahoo.com>
2019-03-17 01:56:12 +0200

    Start.java modification and starting in server

--- src/main/java/Main/Start.java
+++ src/main/java/Main/Start.java
@@ -1,7 +1,14 @@
 package Main;
 
-public class Start {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
-    }
+public class Start {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		String propertiesFile = args[0];
+		Properties properties = new Properties();
+		properties.load(new FileInputStream(propertiesFile));
+	}
 }
