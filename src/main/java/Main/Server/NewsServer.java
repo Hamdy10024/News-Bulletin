29fd36ab7dda1413e677d4bd8849185399f97627
Mohammed Deifallah <mohammed_deifallah@yahoo.com>
2019-03-18 00:09:35 +0200

    Server modification and hopefully finished

--- src/main/java/Main/Server/NewsServer.java
+++ src/main/java/Main/Server/NewsServer.java
@@ -6,61 +6,30 @@ import java.net.ServerSocket;
 
 import Main.FileObject.SharedObject;
 
-public class NewsServer {
+public class NewsServer implements Runnable {
 
 	private SharedObject<Integer> object;
 	private String port_number;
 	private int num_readers, num_writers;
 
 	public NewsServer(String[] args) throws IOException {
-		object = new SharedObject<Integer>();
 		port_number = args[0];
 		num_readers = Integer.parseInt(args[1]);
 		num_writers = Integer.parseInt(args[2]);
+		if (args.length > 2) {
+			object = new SharedObject<Integer>(Integer.parseInt(args[3]));
+		} else {
+			object = new SharedObject<Integer>();
+		}
 	}
 
-	/**
-	 * Runs server functionality as stated in documents and writes output in
-	 * outputfile.
-	 * 
-	 * @param outputFile
-	 *            File stream to write results in.
-	 * @throws IOException
-	 * @throws NumberFormatException
-	 */
-	public void start(FileOutputStream outputFile) throws NumberFormatException, IOException {
+	@Override
+	public void run() {
 		ServerSocket server_socket = new ServerSocket(Integer.parseInt(port_number));
-	}
-
-	/**
-	 * Runs server functionality as stated in documents and writes output in
-	 * outputfile.
-	 * 
-	 * @param outputFile
-	 *            File name to write results in.
-	 */
-	public void start(String outputFile) throws IOException {
-		start(new FileOutputStream(outputFile));
-	}
-
-	/**
-	 * Runs server functionality as stated in documents and writes output in
-	 * outputfile with initial object value.
-	 * 
-	 * @param outputFile
-	 *            File stream to write results in.
-	 * @param initial
-	 *            initial value
-	 * @throws IOException
-	 * @throws NumberFormatException
-	 */
-	public void start(FileOutputStream outputFile, Integer initial) throws NumberFormatException, IOException {
-		object = new SharedObject<Integer>(initial);
-		start(outputFile);
-
-	}
-
-	public void start(String outputFile, Integer initial) throws IOException {
-		start(new FileOutputStream(outputFile), initial);
+		int clients = num_readers + num_writers;
+		for (int i = 0; i < clients; i++) {
+			ClientHandler h = new ClientHandler(server_socket.accept(), object, readers);
+			h.start();
+		}
 	}
 }
